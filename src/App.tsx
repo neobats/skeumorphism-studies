c27a5c262f3e8b9223950b9ce6c464fcc2422df5
Guy Batton <41213716+neobats@users.noreply.github.com>
2022-03-24 22:33:17 -0400

    Hey look it works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react"
 import "./styles.css"
+import playingReset from "./assets/audio-that-i-literally-recorded-on-my-mac/resetClick.wav"
+import playingTest from "./assets/audio-that-i-literally-recorded-on-my-mac/testClick.wav"
 import { Buttons, Receptacle } from "./components"
 
 // finite state machines for stateful aspects
 type LightSquareStates = "off" | "ok" | "err"
-type AudioClickStates = "off" | "playingReset" | "playingTest" | "played"
+type AudioClickStates = "off" | "playingReset" | "playingTest"
 const LIGHT_SQUARE_COLORS = {
   off: "#000",
   ok: "rgba(0, 255, 0, 0.8)",
@@ -50,6 +52,24 @@ export default function App() {
   }
 
   // Effectful stuff
+  useEffect(() => {
+    const getButton = (cl: string) => document.querySelector("button." + cl)
+    const test = getButton("test")
+    const reset = getButton("reset")
+
+    if (tripped) {
+      // will run on initial load, FYI
+      test?.classList.add("on")
+      test?.classList.remove("off")
+      reset?.classList.add("off")
+      reset?.classList.remove("on")
+    } else {
+      reset?.classList.add("on")
+      reset?.classList.remove("off")
+      test?.classList.add("off")
+      test?.classList.remove("on")
+    }
+  }, [tripped])
 
   // Setting the Square Color
   useEffect(() => {
@@ -70,23 +90,32 @@ export default function App() {
     return
   }, [tripped, error])
 
+  // Handling the Audio
   useEffect(() => {
     if (audioClickState === "off") {
       return // don't do anything. Think of this as the recursive base case
-    } else if (
-      audioClickState === "playingReset" ||
-      audioClickState === "playingTest"
-    ) {
-      console.log(audioClickState) // we don't want to do anything
-    } else {
-      // played
-      // turn the duration back to 0, then set the playing state to off.
-      setAudioClickState("off")
     }
+    // this re-runs and we're now "playing", so we want to
+    // actually play the audio file here.
+    const audioElement: HTMLMediaElement | null = document.querySelector(
+      `#${audioClickState}`
+    )
+    if (!audioElement) {
+      setError(true)
+      console.error(
+        "encountered an error playing the audio for",
+        audioClickState
+      )
+      setAudioClickState("off")
+      return
+    }
+    audioElement.play().then(() => {
+      setAudioClickState("off")
+    })
   }, [audioClickState])
 
   return (
-    <>
+    <div className="App">
       <div className="outer">
         <hr />
         <main className="fl-col bg">
@@ -104,6 +133,28 @@ export default function App() {
           </span>
         </p>
       </article>
-    </>
+      <audio
+        src={playingReset}
+        preload="auto"
+        aria-label="reset click sound"
+        id="playingReset"
+      >
+        Well, this is awkward. Your browser does not seem to handle audio. You
+        can try
+        <a href={playingReset}>this link</a>
+        to hear the click instead.
+      </audio>
+      <audio
+        src={playingTest}
+        preload="auto"
+        aria-label="test click sound"
+        id="playingTest"
+      >
+        Well, this is awkward. Your browser does not seem to handle audio. You
+        can try
+        <a href={playingTest}>this link</a>
+        to hear the click instead.
+      </audio>
+    </div>
   )
 }
