40c889e7e365732b30512a6c91c15e29409c883a
Guy Batton <41213716+neobats@users.noreply.github.com>
2022-04-04 10:49:27 -0400

    initial commit

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import { MarkdownPage } from "../components"
 import { useTitle } from "../hooks/useTitle"
 import "./pages.css"
@@ -6,21 +6,31 @@ import "./pages.css"
 type Props = {
   source: string
   closePage: () => void
-  title: string
+  isShowing: boolean
 }
 
-export const Page: React.FC<Props> = ({ source, closePage, title }) => {
-  useTitle(title)
+export const Page: React.FC<Props> = ({ source, closePage, isShowing }) => {
+  const title = useRef("UX Case Study")
+  useTitle(title.current)
 
   useEffect(() => {
     // Sets up relative links for the table of contents
-    document.querySelector("h1")?.setAttribute("id", title.toLocaleLowerCase())
-    document.querySelectorAll("h2, h3, h4").forEach((element) => {
+    if (!isShowing) {
+      return
+    }
+    document.querySelectorAll("h1, h2, h3, h4").forEach((element) => {
       const ele = element as HTMLHeadingElement // stupid. There's a better way to do this.
+
+      if (ele.tagName === "H1") {
+        title.current = ele.innerText
+      }
+
       const headingTitle = ele.innerText
+        .replace(/(\s)/g, "-")
+        .replace(/(\W)/gi, "")
       element.setAttribute("id", headingTitle.toLocaleLowerCase())
     })
-  }, [title])
+  }, [isShowing])
   return (
     <article className="page">
       <button onClick={closePage} className="btn">
